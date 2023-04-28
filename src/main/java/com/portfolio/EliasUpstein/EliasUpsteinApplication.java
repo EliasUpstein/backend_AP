3082866d36357b6d2e01786eab3c2483b6edc616
Elias Upstein <eliasupstein@gmail.com>
2023-04-28 20:53:11 -0300

    Creación del proyecto

---
+++ src/main/java/com/portfolio/EliasUpstein/EliasUpsteinApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.EliasUpstein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliasUpsteinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EliasUpsteinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/EliasUpstein/EliasUpsteinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.EliasUpstein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliasUpsteinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
