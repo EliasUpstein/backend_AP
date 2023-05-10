fed57738ef0d031c474640e1d5509c86b4508f4b
Elias Upstein <eliasupstein@gmail.com>
2023-05-10 00:07:26 -0300

    cambio web config

--- src/main/java/com/portfolio/EliasUpstein/WebConfig.java
+++ src/main/java/com/portfolio/EliasUpstein/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/*
 @EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -13,3 +14,4 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**");
     }
 }
+*/
