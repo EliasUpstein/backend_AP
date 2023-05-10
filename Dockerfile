665f627161c0ef48d706802ee82a3045b84e5725
Elias Upstein <eliasupstein@gmail.com>
2023-05-10 20:44:53 -0300

    solución de errores

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:20
 MAINTAINER EliasUpstein
 COPY target/EliasUpstein-0.0.1-SNAPSHOT.jar portback-app.jar
-EXPOSE 8080
+#EXPOSE 8080
 ENTRYPOINT ["java", "-jar", "/portback-app.jar"]
--- src/main/java/com/portfolio/EliasUpstein/EliasUpsteinApplication.java
+++ src/main/java/com/portfolio/EliasUpstein/EliasUpsteinApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EliasUpsteinApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EliasUpsteinApplication.class, args);
 	}
-
 }
